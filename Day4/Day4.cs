69b29e72e313d4a438c121f0d57e06c3f1e9d6e9
Chrisvasa <Christophervasankari@gmail.com>
2024-08-07 14:34:13 +0200

    "Day 4 part 2"

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -4,17 +4,23 @@ public class Day4
 {
     private static string[] _lines;
     private static int _maxLength;
-    private static int sum = 0;
 
+    // Will refactor to improve performance.. this is a disaster but it works and was fast to code :D
     public static void Scratchcards(string path)
     {
         ReadFile(path);
+        int sum = 0;
+        int bumSum = 0;
+        int[] timesToRun = new int[_maxLength];
+        
         for (int i = 0; i < _maxLength; i++)
         {
             int count = 0;
+            int index = 1;
             string[] temp = _lines[i].Split(':')[1].Split('|');
             string[] numbers = temp[0].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
             string[] winning = temp[1].Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            timesToRun[i]++;
 
             foreach (var num in numbers)
             {
@@ -24,11 +30,14 @@ public class Day4
                         count++;
                     else
                         count *= 2;
+                    timesToRun[i + index++] += 1 * timesToRun[i];
                 }
             }
+            bumSum += timesToRun[i];
             sum += count;
         }
         Console.WriteLine(sum);
+        Console.WriteLine(bumSum);
     }
     
     private static void ReadFile(string path)
